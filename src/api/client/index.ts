5c15ec3af4eeb44766661d75ec002b4e74056a3a
Maksym <maksym@MacBook-Pro-Alina.local>
2024-09-25 15:28:36 +0300

    Fixed requests

--- src/api/client/index.ts
+++ src/api/client/index.ts
@@ -1,9 +1,19 @@
 import {ItemType} from '@src/redux/slices/productsSlice';
+import {Platform} from 'react-native';
+import * as fromJSON from 'db.json';
 
 export async function fetchProducts() {
-  const products: ItemType[] = await fetch(
-    'http://localhost:3000/products',
-  ).then(res => res.json());
-  console.log(products);
-  return products;
+  // ADDED THIS LOGIC BECAUSE json-server doesn't work correct on Android
+  // https://github.com/facebook/react-native/issues/33217
+  // https://github.com/facebook/react-native/issues/32931
+
+  if (Platform.OS === 'ios') {
+    const products: ItemType[] = await fetch(
+      'http://localhost:3000/products',
+    ).then(res => res.json());
+    return products;
+  } else {
+    const {products} = fromJSON;
+    return products;
+  }
 }
